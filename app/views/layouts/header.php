94f1bb456ebf55a10daff4d1bd21b94e8584ad3e
Boris Brovin <45284853+Brovin@users.noreply.github.com>
2019-05-30 09:14:42 +0300

    add block header

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -4,34 +4,44 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="../../../web/css/normalize.css">
-  <link rel="stylesheet" href="../../../web/css/main.css">
+  <link rel="stylesheet" href="/web/css/normalize.css">
+  <link rel="stylesheet" href="/web/css/main.css">
   <title>Flower-Shop</title>
 </head>
 <body>
   <header class="header">
     <div class="container">
-      <nav class="nav">
-        <ul class="nav__list">
-          <li class="nav__item">
-            <a href="#" class="nav__link">Главная</a>
+      <div class="header__wrapper">
+        <nav class="nav">
+          <ul class="nav__list">
+            <li class="nav__item">
+              <a href="/" class="nav__link">Главная</a>
+            </li>
+            <li class="nav__item">
+              <a href="#" class="nav__link">Главная</a>
+            </li>
+            <li class="nav__item">
+              <a href="#" class="nav__link">Главная</a>
+            </li>
+            <li class="nav__item">
+              <a href="#" class="nav__link">Главная</a>
+            </li>
+            <li class="nav__item">
+              <a href="#" class="nav__link">Главная</a>
+            </li>
+            <li class="nav__item">
+              <a href="#" class="nav__link">Главная</a>
+            </li>
+          </ul>
+        </nav>
+        <ul class="auth__list">
+          <li class="auth__item">
+            <a href="#" class="auth__link auth__link_login">Вход</a>
           </li>
-          <li class="nav__item">
-            <a href="#" class="nav__link">Главная</a>
-          </li>
-          <li class="nav__item">
-            <a href="#" class="nav__link">Главная</a>
-          </li>
-          <li class="nav__item">
-            <a href="#" class="nav__link">Главная</a>
-          </li>
-          <li class="nav__item">
-            <a href="#" class="nav__link">Главная</a>
-          </li>
-          <li class="nav__item">
-            <a href="#" class="nav__link">Главная</a>
+          <li class="auth__item">
+            <a href="/registration" class="auth__link auth__link_registration">Регистрация</a>
           </li>
         </ul>
-      </nav>
+      </div>
     </div>
   </header>
