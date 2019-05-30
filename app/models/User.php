7f0a9aab215c35537fd0cb205df577ffe11ec5e2
Boris Brovin <45284853+Brovin@users.noreply.github.com>
2019-05-30 05:06:45 +0300

    update

--- app/models/User.php
+++ app/models/User.php
@@ -3,9 +3,9 @@ class User {
   public static function registration($login, $password) {
     $db = ConnectDb::getConnection();
     $sql = "INSERT INTO users(login, password) VALUES (:login, :password)";
-    $result = $db->prepare($sql);
-    $result->bindParam(':login', $login);
-    $result->bindParam(':password', $password);
-    return $result->execute();
+    $statement = $db->prepare($sql);
+    $statement->bindParam(':login', $login);
+    $statement->bindParam(':password', $password);
+    return $statement->execute();
   }
 }
--- app/views/site/registration.php
+++ app/views/site/registration.php
@@ -2,6 +2,6 @@
 <form action="#" method="post">
   <input type="text" name="login" placeholder="login"/><br>
   <input type="password" name="password" placeholder="password"/><br>
-  <input type="submit" name="submit" value="Регистрация" />
+  <button type="submit" name="submit">Регистрация</button>
 </form>
 <?php require_once __DIR__ . '/../layouts/footer.php'; ?>
