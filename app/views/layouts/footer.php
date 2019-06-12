36d6ffc16d5a0b7f301e91aa9b22aa3d2093b23e
Boris <fody@protonmail.com>
2019-06-12 19:58:26 +0300

    update

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -1,5 +1,7 @@
   <footer class="footer">
-    <p class="footer__content"></p>
+    <p class="footer__content">
+      2019
+    </p>
   </footer>
   <script type="text/javascript" src="/web/js/main.js"></script>
 </body>
