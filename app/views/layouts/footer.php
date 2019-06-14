bfa1c34cc38f5e9ecc0a3dc0be2b9ffd3db0a1fe
Boris Brovin <45284853+Brovin@users.noreply.github.com>
2019-06-14 21:54:26 +0300

    update

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -1,7 +1,5 @@
   <footer class="footer">
-    <p class="footer__content">
-      Сайт был создан в 2019 году
-    </p>
+    <p class="footer__content">Сайт был создан в 2019 году</p>
   </footer>
   <script type="text/javascript" src="/web/js/main.js"></script>
 </body>
