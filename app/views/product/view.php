bda0927e3bacb07d9ea8fb60fcaf64d90797b1a9
Boris <fody@protonmail.com>
2019-06-16 23:45:07 +0300

    update

--- app/views/product/view.php
+++ app/views/product/view.php
@@ -2,10 +2,15 @@
 <main class="main">
   <div class="container">
     <div class="product-view">
-       <img class="product-view__image" src="/web/images/<?php echo $product['image'] ?>" alt="">
-       <h3 class="product-view__title"><?php echo $product['title'] ?></h3>
-       <h2 class="product-view__price">Цена: <?php echo $product['price'] ?> ₽</h2>
-       <a class="product-view__order">Заказать</a>
+      <div class="product-view-left">
+       <img class="product-view-left__image" src="/web/images/<?php echo $product['image'] ?>" alt="">
+      </div>
+      <div class="product-view-right">
+       <h2 class="product-view-right__title">Название: <?php echo $product['title'] ?></h2>
+        <h3 class="product-view-right__article">Артикул: <?php echo $product['code'] ?></h3>
+       <h3 class="product-view-right__price">Цена: <?php echo $product['price'] ?> ₽</h3>
+       <a href="#" class="product-view-right__order">Заказать</a>
+      </div>
     </div>
   </div>
 </main>
