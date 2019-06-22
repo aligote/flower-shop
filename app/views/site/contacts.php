70eb59a44828a6d16752f6fdc8de1594b0b4ed09
Boris Brovin <45284853+Brovin@users.noreply.github.com>
2019-06-22 20:18:08 +0300

    update contacts and about us

--- app/views/site/contacts.php
+++ app/views/site/contacts.php
@@ -2,7 +2,19 @@
   <main class="main">
     <div class="contacts">
       <div class="container">
-
+        <div class="contacts__content">
+          <h2>Интернет-магазин "Flower-Shop"</h2>
+          <p>Контактная информация:
+            Москва, ул. Пушкина, 7</p>
+          <span>Телефон 1</span>
+          <a href="tel:+74957757575">+7(495)775-75-75</a><br>
+          <span>Телефон 2</span>
+          <a href="tel:+74953753535">+7(495)375-35-35</a><br>
+          <span>Email: </span>
+          <a href="mailto:mail@example.com">mail@example.com</a>
+          <h4>Время работы</h4>
+          <p>Заказ на сайте можно оформить круглосуточно. Обработка и приём заказов по телефону осуществляется ежедневно круглосуточно. Доставка обработанных заказов осуществляется </p>
+        </div>
       </div>
     </div>
   </main>
