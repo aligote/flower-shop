3f152cb28bdbacb80666d4e8e24b6ef13578ba43
Boris <fody@protonmail.com>
2019-07-21 10:13:04 +0300

    update footer

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -3,6 +3,6 @@
       <p class="footer__content">Сайт курсовой работы 2019 года</p>
     </div>
   </footer>
-  <script type="text/javascript" src="/web/js/main.js"></script>
+  <script src="/web/js/main.js"></script>
 </body>
 </html>
